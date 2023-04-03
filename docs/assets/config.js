ad1e59c8c712af9d416aef42c6285db5476ded70
Shiina <shiinahiiragi@outlook.com>
2023-04-03 15:47:20 +0800

    stage

--- docs/assets/config.js
+++ docs/assets/config.js
@@ -23,3 +23,24 @@ window.MathJax = {
         scale: 0.94
     }
 };
+
+const changeFont = (isSerif) => {
+    const changeVar = (key, value) => {
+        document.querySelector(":root").style.setProperty(key, value);
+    }
+
+    if (isSerif) {
+        changeVar("--md-text-font", "\"Noto Serif SC\"");
+        changeVar("--normal-font-weight", "500");
+        changeVar("--bold-font-weight", "700");
+        changeVar("--title-font-weight", "500");
+    } else {
+        changeVar("--md-text-font", "\"Noto Sans\", \"Noto Sans SC\"");
+        changeVar("--normal-font-weight", "400");
+        changeVar("--bold-font-weight", "600");
+        changeVar("--title-font-weight", "300");
+    }
+}
+
+// execute in each page
+document.querySelectorAll(".year").forEach((item) => item.innerText = new Date().getFullYear());
